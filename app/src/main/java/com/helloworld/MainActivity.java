a695d4d5f02e5bb494456742ec9674d87452528f
kkampfen <kkampfen@outlook.com>
2015-09-05 15:27:34 -0700

    v2

--- app/src/main/java/com/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/MainActivity.java
@@ -1,43 +1,61 @@
 package com.helloworld;
 
 //import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import java.lang.String;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
-    TextView button;
-    TextView label;
-    int count = 0;
+    Button button1, button2;
+    TextView label1, label2;
+    int count1 = 0, count2 = 0;
+    String times = " time";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (TextView) findViewById(R.id.button);
-        label = (TextView) findViewById(R.id.label);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        label1 = (TextView) findViewById(R.id.label1);
+        label2 = (TextView) findViewById(R.id.label2);
 
-        button.setOnClickListener(global_ClickListener);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+        label1.setTextColor(Color.BLUE);
+        label2.setTextColor(Color.BLUE);
     }
 
-    final OnClickListener global_ClickListener = new OnClickListener() {
-            public void onClick(final View v) {
-                count++;
+    @Override
+    public void onClick(View v) {
 
-                if(count % 2 != 0) {
-                    label.setText("Hello world");
-                }
-                else {
-                    label.setText("Click the button");
-                }
+        if(v.getId() == R.id.button1) {
+            count1++;
+            if (count1 > 1) {
+                times = " times";
             }
-    };
+            label1.setText("Top button");
+            label2.setText("pushed " + count1 + times);
+        }
+        else if (v.getId() == R.id.button2){
+            count2++;
+            if (count2 > 1){
+                times = " times";
+            }
+            label1.setText("Bottom button");
+            label2.setText("pushed " + count2 + times);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
